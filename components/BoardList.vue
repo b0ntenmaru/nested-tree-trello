94d7e69c572146172546a9039a7ac9b45eaf9abb
Hiroaki Date <mmst3l4@gmail.com>
2020-12-06 01:14:23 +0900

    trello dummyコンポーネントを作成

---
+++ components/BoardList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <Draggable
+      group="boards"
+      draggable=".board-list"
+      handle=".handle"
+      :animation="300"
+      class="draggable-wrapper"
+    >
+      <div v-for="(board, index) in value" :key="index" class="board-list">
+        <div class="handle board-list-lane">
+          {{ board.title }}
+        </div>
+
+        <Draggable group="childs" :animation="300">
+          <div v-for="(child, index) in board.childs" :key="index">
+            {{ child.name }}
+          </div>
+        </Draggable>
+      </div>
+    </Draggable>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Draggable from 'vuedraggable';
+
+export default Vue.extend({
+  name: 'BoardList',
+
+  components: {
+    Draggable,
+  },
+
+  props: {
+    value: {},
+  },
+
+  methods: {
+    emitter(value: any) {
+      this.$emit('input', value);
+    },
+  },
+});
+</script>
+
+<style>
+div.draggable-wrapper {
+  display: flex;
+}
+
+div.board-list {
+  width: 100%;
+  display: inline-block;
+  text-align: center;
+  width: 34%;
+}
+
+div.board-list-lane {
+  background: #ccc;
+}
+</style>
